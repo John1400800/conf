5f6a5003b4dca2aa2da51ace14230037de8fc5c9
Johnchic <94189388+John1400800@users.noreply.github.com>
2024-04-23 17:03:09 +0300

    Create slstatus-config.h

---
+++ slstatus-config.h
@@ -0,0 +1,10 @@
+static const struct arg args[] = {
+        /* function format          argument */
+        { cpu_perc,      "CPU: %s%% | ", NULL           },
+        { ram_perc,      "RAM: %s%% | ", NULL           },
+        { wifi_perc,     "NET: %s%% | ", "wlp0s20f3"    },
+        { battery_state, "BAT: %s",      "BAT1"         },
+        { battery_perc,  "%s%% | ",      "BAT1"         },
+        { keymap,        "%s | ",        NULL           },
+        { datetime,      "%s",           "%a %d %b %T"  },
+};
