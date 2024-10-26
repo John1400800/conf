46e17338d453727e97ed754ed00ac94aec7b91b4
John <johserheiv@gmail.com>
2024-10-26 22:24:03 +0300

    update sway colors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,6 +11,8 @@ vim.opt.smartcase = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.scrolloff = 5
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.api.nvim_set_keymap('n', '<F4>', ':nohlsearch<CR>', { noremap = true, silent = true })
 
@@ -28,5 +30,14 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
     vim.opt_local.makeprg = "g++ %"
+  end,
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, 't', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
   end,
 })
